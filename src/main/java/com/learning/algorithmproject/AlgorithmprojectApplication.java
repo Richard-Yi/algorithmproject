154302a2a4bdb41b36eeb4249ceef6d9f834f338
Richard_yyf <yyf_richard@outlook.com>
2018-08-25 14:22:47 +0800

    Initial commit

---
+++ src/main/java/com/learning/algorithmproject/AlgorithmprojectApplication.java
@@ -0,0 +1,12 @@
+package com.learning.algorithmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmprojectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/learning/algorithmproject/chapter1Base/Test.java
@@ -0,0 +1,18 @@
+package com.learning.algorithmproject.chapter1Base;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Richard_yyf
+ * @Date 2018/8/25
+ * @Description
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("tets","1");
+    }
+}
