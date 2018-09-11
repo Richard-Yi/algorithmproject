2613f1d2246d1a66b984fdf04e63c51e39d2eecb
Richard_yyf <yyf_richard@outlook.com>
2018-09-11 13:17:21 +0800

    上传代码：用链表来表示多项式，并且实现多项式相乘和相加操作。
    Signed-off-by: Richard_yyf <yyf_richard@outlook.com>

--- src/main/java/com/learning/algorithmproject/chapter2collections/Week2Test1.java
+++ src/main/java/com/learning/algorithmproject/chapter2collections/Week2Test1.java
@@ -5,60 +5,64 @@ import java.util.Scanner;
 /**
  * @author Richard_yyf
  * @Date 2018/8/29
- * @Description 本题要求实现一个函数，将两个链表表示的递增整数序列合并为一个非递减的整数序列。
+ * @Description 设计函数分别求两个一元多项式的乘积与和。
  * <p>
- * 函数接口定义：
- * List Merge( List L1, List L2 );
+ * 输入格式:
+ * 输入分2行，每行分别先给出多项式非零项的个数，再以指数递降方式输入一个多项式非零项系数和指数（绝对值均为不超过1000的整数）。数字间以空格分隔。
  * <p>
- * L1和L2是给定的带头结点的单链表，其结点存储的数据是递增有序的；
- * 函数Merge要将L1和L2合并为一个非递减的整数序列。应直接使用原序列中的结点，返回归并后的带头结点的链表头指针。
+ * 输出格式:
+ * 输出分2行，分别以指数递降方式输出乘积多项式以及和多项式非零项的系数和指数。数字间以空格分隔，但结尾不能有多余空格。零多项式应输出[0] [0]。
  * <p>
- * 输入样例：
- * 3
- * 1 3 5
- * 5
- * 2 4 6 8 10
+ * 输入样例:
+ * 4 3 4 -5 2  6 1  -2 0
+ * 3 5 20  -7 4  3 1
  * <p>
- * 输出样例：
- * 1 2 3 4 5 6 8 10
- * NULL
- * NULL
+ * 输出样例:
+ * 15 24 -25 22 30 21 -10 20 -21 8 35 6 -33 5 14 4 -15 3 18 2 -6 1
+ * 5 20 -4 4 -5 2 9 1 -2 0
  */
 public class Week2Test1 {
 
-    /**
-     * 测试程序
-     *
-     * @param args
-     */
     public static void main(String[] args) {
-        int size1, size2;
+        int k;
+        // 系数
+        int coef;
+        // 指数
+        int expn;
         Scanner in = new Scanner(System.in);
-        size1 = in.nextInt();
-        Node head1 = null, next = null;
-        for (int i = 0; i < size1; i++) {
+        //输入序列的长度
+        System.out.println("please input a number");
+        k = in.nextInt();
+        //输入序列
+        System.out.println("please input the numbers");
 
+        PolyList polyList1 = new PolyList();
+        polyList1.theSize = k;
+        for (int i = 0; i < k; i++) {
+            coef = in.nextInt();
+            expn = in.nextInt();
+            polyList1.add(coef, expn);
         }
-        System.out.println(head1.value);
+
+        //输入序列的长度
+        System.out.println("please input a number");
+        k = in.nextInt();
+        //输入序列
+        System.out.println("please input the numbers");
+
+        PolyList polyList2 = new PolyList();
+        polyList2.theSize = k;
+        for (int i = 0; i < k; i++) {
+            coef = in.nextInt();
+            expn = in.nextInt();
+            polyList1.add(coef, expn);
+        }
+        // 多项式相加
+        PolyList.addPoly(polyList1, polyList2).print();
+        // 多项式相乘
+        PolyList.multiplyPoly(polyList1, polyList2).print();
+
     }
 
-    private static class Node {
-
-        /**
-         * 指向下一结点
-         */
-        Node next;
-
-        /**
-         * 本结点的值
-         */
-        int value;
-
-        Node(Node next, int value) {
-            super();
-            this.next = next;
-            this.value = value;
-        }
-    }
 
 }
