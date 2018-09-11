ab07af536b79201f1a024d40a9c2f1612b31ac4f
Richard_yyf <yyf_richard@outlook.com>
2018-09-11 19:14:26 +0800

    整合代码成PTA提交的样式
    Signed-off-by: Richard_yyf <yyf_richard@outlook.com>

--- src/main/java/com/learning/algorithmproject/chapter2collections/Week2Test1.java
+++ src/main/java/com/learning/algorithmproject/chapter2collections/Week2Test1.java
@@ -31,11 +31,8 @@ public class Week2Test1 {
         int expn;
         Scanner in = new Scanner(System.in);
         //输入序列的长度
-        System.out.println("please input a number");
         k = in.nextInt();
         //输入序列
-        System.out.println("please input the numbers");
-
         PolyList polyList1 = new PolyList();
         polyList1.theSize = k;
         for (int i = 0; i < k; i++) {
@@ -44,12 +41,8 @@ public class Week2Test1 {
             polyList1.add(coef, expn);
         }
 
-        //输入序列的长度
-        System.out.println("please input a number");
         k = in.nextInt();
         //输入序列
-        System.out.println("please input the numbers");
-
         PolyList polyList2 = new PolyList();
         polyList2.theSize = k;
         for (int i = 0; i < k; i++) {
@@ -57,12 +50,163 @@ public class Week2Test1 {
             expn = in.nextInt();
             polyList2.add(coef, expn);
         }
-        // 多项式相加
-        PolyList.addPoly(polyList1, polyList2).print();
         // 多项式相乘
         PolyList.multiplyPoly(polyList1, polyList2).print();
+        // 多项式相加
+        PolyList.addPoly(polyList1, polyList2).print();
+    }
+}
 
+class PolyList {
+
+    public PolyNode headNode;
+
+    public PolyNode current;
+
+    public int theSize;
+
+    public PolyList() {
+        super();
+        doClear();
     }
 
+    public void doClear() {
+        this.headNode = new PolyNode();
+        current = headNode;
+    }
 
+    public void add(int coef, int expn) {
+        PolyNode node = new PolyNode(coef, expn);
+        current.next = node;
+        current = node;
+    }
+
+    public class PolyNode {
+        /**
+         * 系数
+         */
+        public int coef;
+        /**
+         * 指数
+         */
+        public int expn;
+        /**
+         * 下一个节点
+         */
+        public PolyNode next;
+
+        public PolyNode() {
+
+        }
+
+        public PolyNode(int coef, int expn) {
+            this.coef = coef;
+            this.expn = expn;
+        }
+    }
+
+    /**
+     * 多项式相加
+     *
+     * @param polyList1
+     * @param polyList2
+     * @return
+     */
+    public static PolyList addPoly(PolyList polyList1, PolyList polyList2) {
+        PolyNode node1 = polyList1.headNode.next;
+        PolyNode node2 = polyList2.headNode.next;
+        PolyList resultPolyList = new PolyList();
+
+        while (node1 != null && node2 != null) {
+            int expn1 = node1.expn;
+            int expn2 = node2.expn;
+            int coef1 = node1.coef;
+            int coef2 = node2.coef;
+
+            if (expn1 == expn2) {
+                // 指数相同，系数相加
+                if (coef1 + coef2 != 0) {
+                    resultPolyList.add(coef1 + coef2, expn1);
+                }
+                node1 = node1.next;
+                node2 = node2.next;
+            } else if (expn1 < expn2) {
+                // node2 先放进结果多项式的链表中
+                resultPolyList.add(coef2, expn2);
+                node2 = node2.next;
+            } else {
+                // node1 指数项较大
+                resultPolyList.add(coef1, expn1);
+                node1 = node1.next;
+            }
+        }
+        // 多项式一已经结束了
+        while (node2 != null) {
+            resultPolyList.add(node2.coef, node2.expn);
+            node2 = node2.next;
+        }
+        // 多项式二已经结束了
+        while (node1 != null) {
+            resultPolyList.add(node1.coef, node1.expn);
+            node1 = node1.next;
+        }
+        return resultPolyList;
+    }
+
+    /**
+     * 多项式相乘
+     *
+     * @param polyList1 多项式1
+     * @param polyList2 多项式2
+     * @return 结果多项式
+     */
+    public static PolyList multiplyPoly(PolyList polyList1, PolyList polyList2) {
+        PolyNode node1 = polyList1.headNode.next;
+        PolyList resultPolyList = new PolyList();
+        while (node1 != null) {
+            resultPolyList = addPoly(resultPolyList, multiplyPoly(node1, polyList2));
+            node1 = node1.next;
+        }
+        return resultPolyList;
+    }
+
+    private static PolyList multiplyPoly(PolyNode mNode, PolyList polyList) {
+        PolyNode node = polyList.headNode.next;
+        PolyList resultPolyList = new PolyList();
+        while (node != null) {
+            resultPolyList.add(node.coef * mNode.coef, node.expn + mNode.expn);
+            node = node.next;
+        }
+        return resultPolyList;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        PolyNode node = headNode.next;
+        while (node != null) {
+            sb.append(node.coef).append("x^").append(node.expn);
+            sb.append(" + ");
+            node = node.next;
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    /**
+     * 根据题目要求打印
+     */
+    public void print() {
+        PolyNode node = headNode.next;
+        StringBuilder sb = new StringBuilder();
+        if (node == null) {
+            // 零多项式
+            sb.append(0).append(" ").append(0);
+        } else {
+            while (node != null) {
+                sb.append(node.coef).append(" ").append(node.expn).append(" ");
+                node = node.next;
+            }
+        }
+        System.out.println(sb.substring(0, sb.length() - 1));
+    }
 }
